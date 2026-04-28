18676341ba41ad199ba8d458a04177be99b37384
DatNT <nguyentiendat1125@gmail.com>
2026-04-28 23:17:10 +0700

    module-jobs

--- project-job-hunter/backend/src/main/java/vn/demo/jobhunter/domain/response/job/ResCreateJobDTO.java
+++ project-job-hunter/backend/src/main/java/vn/demo/jobhunter/domain/response/job/ResCreateJobDTO.java
@@ -1,11 +1,11 @@
 package vn.demo.jobhunter.domain.response.job;
 
 import java.time.Instant;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import vn.demo.jobhunter.util.constant.LevelEnum;
 
-// DTO trả về sau khi TẠO Job thành công
-// Chỉ chứa những trường cần thiết, không trả toàn bộ Entity
 @Getter
 @Setter
 public class ResCreateJobDTO {
@@ -14,9 +14,11 @@ public class ResCreateJobDTO {
     private String location;
     private double salary;
     private int quantity;
+    private LevelEnum level;
     private Instant startDate;
     private Instant endDate;
     private boolean isActive;
+    private List<String> skills;
     private Instant createdAt;
     private String createdBy;
 }
