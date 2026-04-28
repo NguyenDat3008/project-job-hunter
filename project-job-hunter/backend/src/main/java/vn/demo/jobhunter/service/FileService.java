2b1165002761358931001208bf3f476bbea59dd0
Bui Minh Quan <bmq1603@gmail.com>
2026-04-29 01:50:35 +0700

    feat add ReqChangePasswordDTO

---
+++ project-job-hunter/backend/src/main/java/vn/demo/jobhunter/service/FileService.java
@@ -0,0 +1,25 @@
+package vn.demo.jobhunter.service;
+
+import java.io.IOException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileService {
+
+    private final MinioService minioService;
+
+    public FileService(MinioService minioService) {
+        this.minioService = minioService;
+    }
+
+    public String store(MultipartFile file, String folder) throws Exception {
+        String finalName = System.currentTimeMillis() + "-" + file.getOriginalFilename();
+        minioService.uploadFile(finalName, file.getInputStream(), file.getSize(), file.getContentType());
+        return finalName;
+    }
+
+    public String getFileUrl(String fileName) {
+        return minioService.getFileUrl(fileName);
+    }
+}
